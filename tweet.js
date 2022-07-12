a2559abc7ccf07b5e42d03832fb21bcbe82a93bb
Derek Cooper <derekcooper20@gmail.com>
2022-07-12 15:43:12 -0600

    Adds code

--- tweet.js
+++ tweet.js
@@ -1,5 +1,25 @@
 class Tweet {
+constructor(userName, statement, link) {
+    this.sender = userName;
+    this.content = statement;
+    this.link = link || 'no link provided';
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+}
 
+    like() {
+         this.likes += 1;
+    }
+
+    retweet() {
+        this.retweets += 1;
+    }
+    reply(replies) {
+        this.replies.push(replies);
+        this.replyCount += 1;
+    }
 }
 
 module.exports = Tweet;
